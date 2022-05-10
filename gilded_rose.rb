f5a2a999de31a11f942f14caa9dcade89d6d81f6
Kristin Brooks <kristin.ruth.brooks@gmail.com>
2022-05-09 18:22:06 -0700

    extract generic object

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,5 +1,26 @@
 class GildedRose
 
+  class Generic
+    attr_reader :quality, :sell_in
+
+    def initialize(quality, sell_in)
+      @quality, @sell_in = quality, sell_in
+    end
+
+    def update
+      if @quality > 0
+        @quality = @quality - 1
+      end
+      @sell_in = @sell_in - 1
+      if @sell_in < 0
+        if @quality > 0
+          @quality = @quality - 1
+        end
+      end
+    end
+
+  end
+
   def initialize(items)
     @items = items
   end
@@ -8,15 +29,10 @@ class GildedRose
     @items.each do |item|
       if sulfuras?(item)
       elsif generic?(item)
-        if item.quality > 0
-          item.quality = item.quality - 1
-        end
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0
-          if item.quality > 0
-            item.quality = item.quality - 1
-          end
-        end
+        generic = Generic.new(item.quality, item.sell_in)
+        generic.update
+        item.quality = generic.quality
+        item.sell_in = generic.sell_in
       elsif aged_brie?(item)
         if item.quality < 50
           item.quality = item.quality + 1
