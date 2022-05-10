650b0564358cc050f1bff458a07aa4f27fe69d9e
Kristin Brooks <kristin.ruth.brooks@gmail.com>
2022-05-09 17:35:58 -0700

    flatten the else body when it only contains another if/else

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -21,19 +21,17 @@ class GildedRose
         handle_backstage_pass(item)
       end
       if item.sell_in < 0
-        if ! aged_brie?(item)
-          if backstage_pass?(item)
-            item.quality = item.quality - item.quality
-          else
-            if item.quality > 0
-              if ! sulfuras?(item)
-                decrease_quality(item)
-              end
-            end
-          end
-        else
+        if aged_brie?(item)
           if quality_less_than_50(item)
             increase_quality(item)
+          end
+        elsif backstage_pass?(item)
+          item.quality = item.quality - item.quality
+        else
+          if item.quality > 0
+            if ! sulfuras?(item)
+              decrease_quality(item)
+            end
           end
         end
       end
