33b2d45662826540660d2d541ac75b0d4ea698b0
Kristin Brooks <kristin.ruth.brooks@gmail.com>
2022-05-11 01:19:12 -0700

    extract backstage pass

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -40,6 +40,34 @@ class GildedRose
     end
   end
 
+  class BackstagePass
+    attr_reader :quality, :sell_in
+
+    def initialize(quality, sell_in)
+      @quality, @sell_in = quality, sell_in
+    end
+
+    def update
+      if @quality < 50
+        @quality = @quality + 1
+        if @sell_in < 11
+          if @quality < 50
+            @quality = @quality + 1
+          end
+        end
+        if @sell_in < 6
+          if @quality < 50
+            @quality = @quality + 1
+          end
+        end
+      end
+      @sell_in = @sell_in - 1
+      if @sell_in < 0
+        @quality = @quality - @quality
+      end
+    end
+  end
+
   def initialize(items)
     @items = items
   end
@@ -58,23 +86,10 @@ class GildedRose
         item.quality = aged_brie.quality
         item.sell_in = aged_brie.sell_in
       elsif backstage_pass?(item)
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.sell_in < 11
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-          if item.sell_in < 6
-            if item.quality < 50
-              item.quality = item.quality + 1
-            end
-          end
-        end
-        item.sell_in = item.sell_in - 1
-        if item.sell_in < 0
-          item.quality = item.quality - item.quality
-        end
+        backstage_pass = BackstagePass.new(item.quality, item.sell_in)
+        backstage_pass.update
+        item.quality = backstage_pass.quality
+        item.sell_in = backstage_pass.sell_in
       end
     end
   end
